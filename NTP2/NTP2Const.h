9d718cfae85d9cabeb60febbb2391051df636135
croberson94 <crobshere@gmail.com>
2019-04-03 16:41:39 -0500

    new NTP files

---
+++ NTP2/NTP2Const.h
@@ -0,0 +1,24 @@
+// File generated 4/3/2019 4:07:32 PM
+// Warning! This is a generated file, any manual changes will be
+// lost during the next generation.
+
+#if (ARDUINO >= 100)
+	#include "Arduino.h" // for Arduino 1.0
+#else
+	#include "WProgram.h" // for Arduino 23
+#endif
+
+// object indexes into ImageControl
+#define iWinbutton1             0 // offset 0x0
+#define iWinbutton2             1 // offset 0x1000
+#define iWinbutton3             2 // offset 0x2000
+#define iWinbutton4             3 // offset 0x3000
+#define iWinbutton5             4 // offset 0x4000
+#define iWinbutton6             5 // offset 0x5000
+#define iWinbutton7             6 // offset 0x6000
+#define iWinbutton8             7 // offset 0x7000
+#define iWinbutton9             8 // offset 0x8000
+#define iWinbutton10            9 // offset 0xA200
+#define iWinbutton11           10 // offset 0xC400
+#define iWinbutton12           11 // offset 0xE600
+
